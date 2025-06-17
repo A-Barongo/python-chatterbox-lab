3df5a24cc2f558e33e636054bda8b199012d113a
Allan <allanbarongo5@gmail.com>
2025-06-17 07:27:50 +0300

    Completed chatterbox CRUD lab

--- server/app.py
+++ server/app.py
@@ -14,13 +14,63 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages',methods=['GET', 'POST'])
 def messages():
-    return ''
+    
+    if request.method=='GET':
+        messages=Message.query.order_by(Message.created_at.asc()).all()
+        if not messages:
+            return make_response({"error":"There are no messages at the time"},404)
+        response=make_response(jsonify([message.to_dict() for message in messages]),200)
+        return response
+    
+    elif request.method=='POST':
+        data = request.get_json()
+        body = data.get('body')
+        username = data.get('username')
 
-@app.route('/messages/<int:id>')
+        new_message = Message(body=body, username=username)
+        db.session.add(new_message)
+        db.session.commit()
+        response=make_response(jsonify(new_message.to_dict()),200)
+        return response
+
+@app.route('/messages/<int:id>',methods=['PATCH','DELETE'])
 def messages_by_id(id):
-    return ''
+    message=Message.query.filter_by(id=id).first()
+    if not message:
+        return make_response({"error":f"Message of id:{id} not found"},404)
+    if request.method=='PATCH':
+        data = request.get_json()
+        for attr in data:
+            setattr(message, attr, data[attr])
+
+        db.session.commit()
+
+        message_dict = message.to_dict()
+
+        response = make_response(
+            jsonify(message_dict),
+            200
+        )
+
+        return response
+    
+    elif request.method=='DELETE':
+        db.session.delete(message)
+        db.commit()
+        
+        response_body = {
+        "delete_successful": True,
+        "message": "Message deleted."
+        }
+
+        response = make_response(
+            response_body,
+            200
+        )
+
+        return response
 
 if __name__ == '__main__':
     app.run(port=5555)
